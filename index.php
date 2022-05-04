d1d4454f8dc64b88a84be58d867d7ef3d2c33ece
mohit <mk7738771@gmail.com>
2022-05-04 14:13:35 +0530

    form

--- index.php
+++ index.php
@@ -5,18 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insert Data into Database</title>
+    <link rel="stylesheet" href="New folder/style.css">
 </head>
 <body>
-    <div class="">
+    <div>
         <form class="" action="Insert_into_Database.php" method="post">
             <fieldset>
-                <span>
-                    Employee Name:
-                </span>
+                <span class="FieldInfo"> Employee Names: </span>
                 <br>
                 <input type="text"name="Ename" value="">
                 <br>
+                <span class="FieldInfo">Social Security number  </span>
+                <br>
+                <input type="text"name="SSN" value="">
+                <br>
+                <span class="FieldInfo"> department: </span>
+                <br>
+                <input type="text"name="Dept" value="">
+                <br>
+                <span class="FieldInfo"> Salary: </span>
+                <br>
+                <input type="text"name="Sal" value="">
+                <br>
+                <span class="FieldInfo"> Home address: </span>
+                <br>
+                <textarea name="HoneAddress"cols="80" rows="8"></textarea>
+                <br>
+                <input type="submit" name="Submit" value="Submit your record">
             </fieldset>
+   
         </form>
     </div>
 </body>
