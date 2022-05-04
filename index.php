5181249835189b7bcced5b8df3d1b4cdf9f5df19
mohit <mk7738771@gmail.com>
2022-05-04 12:17:06 +0530

    Insert data

--- index.php
+++ index.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Basic changed</title>
+    <title>Insert Data into Database</title>
 </head>
 <body>
-    
+    <div class="">
+        <form class="" action="Insert_into_Database.php" method="post">
+            <fieldset>
+                <span>
+                    Employee Name:
+                </span>
+                <br>
+                <input type="text"name="Ename" value="">
+                <br>
+            </fieldset>
+        </form>
+    </div>
 </body>
 </html>
